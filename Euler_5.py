63326046d6f8c8dab9ae37b1eec83d9956c0c26d
Pranay Patni <pranaypatni96@berkeley.edu>
2016-07-11 19:08:57 -0700

    Solved problem 5.

--- Euler_5.py
+++ Euler_5.py
@@ -5,13 +5,20 @@ any remainder.
 
 What is the smallest positive number that is evenly 
 divisible by all of the numbers from 1 to 20?
+
+(brute force solution)
 """
 
-def smallest_multiple(n):
+i = 2520
+while True:
+	if ( i%1==0 and i%2==0 and i%3==0 and i%4==0 and
+		 i%5==0 and i%6==0 and i%7==0 and i%8==0 and
+		 i%9==0 and i%10==0 and i%11==0 and i%12==0 and
+		 i%13==0 and i%14==0 and i%15==0 and i%16==0 and
+		 i%17==0 and i%18==0 and i%19==0 and i%20==0):
+			break
+	else:
+		i += 2520
 
-
-x = smallest_multiple(20)
-print(x)
-
-
+print(i)
 
